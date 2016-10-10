24ac11b8a1c915a03a12185f480585d8ee02c130
sujangJin <sujang1@naver.com>
2016-10-10 18:44:34 +0900

    exception handling

--- samsung_2015_summer_Q2.cpp
+++ samsung_2015_summer_Q2.cpp
@@ -4,14 +4,14 @@ using namespace std;
 
 int main()
 {
-	int testcase = 0, index = 0;
+	int testcase = 0, index = 0, allnegative = 1;
 	int **numbers = new int*[10];
 	int minus = 0, minimal = 0;
 	
 	// input the number of datasets
 	cin >> testcase;
 
-	// allocate array & input each integer
+	// allocate array
 	for (int i = 0; i < testcase; i++){
 		numbers[i] = new int[2];
 		cin >> numbers[i][0] >> numbers[i][1];
@@ -19,12 +19,26 @@ int main()
 	
 	// process
 	while (index < testcase){
+		if (numbers[index][0] < 0 && numbers[index][1] < 0){
+			numbers[index][0] *= -1; 
+			numbers[index][1] *= -1;
+			allnegative = -1;
+		}
+		else if (numbers[index][0] < 0){
+			numbers[index][0] *= -1;
+		}
+		else if (numbers[index][1] < 0){
+			numbers[index][1] *= -1;
+		}
+
+		
+		
 		minus = abs(numbers[index][0] - numbers[index][1]);
 		minimal = min(numbers[index][0], numbers[index][1]);
 
 		if (minus == minimal){
 			cout << "#testcase" << index + 1 << endl;
-			cout << minus;
+			cout << minus*allnegative;
 			cout << endl;
 			index++;
 		}
