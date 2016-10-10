593b1f84e7499349c0a082a0d59fad4fdc14e942
sujangJin <sujang1@naver.com>
2016-10-10 18:07:03 +0900

    samsung

---
+++ samsung_2015_summer_Q2.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int main()
+{
+	int testcase = 0, index = 0;
+	int **numbers = new int*[10];
+	int minus = 0, minimal = 0;
+	
+	// input the number of datasets
+	cin >> testcase;
+
+	// allocate array
+	for (int i = 0; i < testcase; i++){
+		numbers[i] = new int[2];
+		cin >> numbers[i][0] >> numbers[i][1];
+	}
+	
+	// process
+	while (index < testcase){
+		minus = abs(numbers[index][0] - numbers[index][1]);
+		minimal = min(numbers[index][0], numbers[index][1]);
+
+		if (minus == minimal){
+			cout << "#testcase" << index + 1 << endl;
+			cout << minus;
+			cout << endl;
+			index++;
+		}
+		else {
+			numbers[index][0] = minus;
+			numbers[index][1] = minimal;
+		}
+	}
+
+	// return the dynamically allocated array
+	for (int i = 0; i < testcase; i++){
+		delete[] numbers[i];
+	}
+	delete[] numbers;
+	return 0;
+}
